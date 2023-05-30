86b9fff8079d56ca7accc691afa6d67b3f5890ae
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-05-30 21:49:13 +0600

    changes in triggers

--- Trigger/Notification/orderUpdateDelivery.sql
+++ Trigger/Notification/orderUpdateDelivery.sql
@@ -1,9 +1,16 @@
 CREATE OR REPLACE FUNCTION order_update_notification2()
 RETURNS TRIGGER AS $$
+DECLARE
   DeliveryEmail varchar(100);
+  result_count INT;
 BEGIN
-
-  INSERT INTO NOTIFICATION(Email,Message) VALUES(NEW.CustomerEmail,'Order Status Has Been Updated to '||NEW.Status||' of OrderID: '||NEW.OrderID);
+  SELECT COUNT(*) INTO result_count FROM DELIVERY_INFORMATION WHERE OrderID=NEW.OrderID;
+  IF(result_count>0) THEN
+    SELECT DELIVERY_INFORMATION.DeliveryEmail INTO DeliveryEmail FROM DELIVERY_INFORMATION WHERE OrderID=NEW.OrderID;
+  ELSE
+    RETURN NEW;
+  END IF;
+  INSERT INTO NOTIFICATION(Email,Message) VALUES(DeliveryEmail,'Order Status Has Been Updated to '||NEW.Status||' of OrderID: '||NEW.OrderID);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
